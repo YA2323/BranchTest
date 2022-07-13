fd1d76447978831068c2ba2aa40ed32cd401e1ba
yacinalkaissouni <yacin.alkaissouni@gmx.de>
2022-07-13 15:55:15 +0200

    Second Branch

---
+++ src/main/java/com/example/branchtest/BranchTest.java
@@ -0,0 +1,15 @@
+package com.example.branchtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class BranchTest {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello, Number2";
+    }
+
+}
